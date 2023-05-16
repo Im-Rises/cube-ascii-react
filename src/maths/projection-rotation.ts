8f8acb0cd88d78cd62f516b7948e86c54b5f1a1a
Im-Rises <quentin-morel88@hotmail.com>
2023-05-16 11:06:36 -0400

    Updated eslint

---
+++ src/maths/projection-rotation.ts
@@ -0,0 +1,23 @@
+// const rotateX = (p, angle) => {
+// 	const c = Math.cos(angle);
+// 	const s = Math.sin(angle);
+// 	const y = p.y * c - p.z * s;
+// 	const z = p.y * s + p.z * c;
+// 	return {...p, y, z};
+// };
+//
+// const rotateY = (p, angle) => {
+// 	const c = Math.cos(angle);
+// 	const s = Math.sin(angle);
+// 	const x = p.x * c - p.z * s;
+// 	const z = p.x * s + p.z * c;
+// 	return {...p, x, z};
+// };
+//
+// const rotateZ = (p, angle) => {
+// 	const c = Math.cos(angle);
+// 	const s = Math.sin(angle);
+// 	const x = p.x * c - p.y * s;
+// 	const y = p.x * s + p.y * c;
+// 	return {...p, x, y};
+// };
